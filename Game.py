725ed731cfa3e44ba7ba9cd94e459df385f59f0b
najehprojects <najeh.ddembe@bcestudent.com>
2025-09-11 16:20:28 +0100

    Finished Character Setup

--- Game.py
+++ Game.py
@@ -8,12 +8,21 @@ def wait(secs):
 
 plr = {
     "name" : "",
-    "pos" : 0,
-    "items" : [""],
     "weapon" : "None",
-    "scene" : "Intro",
+
+    "inventory": [""],
+
     "level" : 1,
     "xp" : 0,
+    "atk" : 1,
+    "def" : 1,
+    "hp" : 10,
+
+    "scene": "Intro",
+    "moves" : 50,
+    "pos": 0,
+
+    "dif" : ""
 }
 
 def showstats():
@@ -45,7 +54,38 @@ while plr["name"] == "":
 
 showstats()
 
-doTut = input("Would you like to play the tutorial? [Y/N]: ")
+def difficultyselect():
 
+    animatetxt("Choose Your Difficulty", 3)
+    plr["dif"] = input("[Easy/Normal/Hard/Impossible] ")
+
+    while plr["dif"].upper() != "EASY" and plr["dif"].upper() != "NORMAL" and plr["dif"].upper() != "HARD" and plr["dif"].upper() != "IMPOSSIBLE" and plr["dif"].upper() != "E" and plr["dif"].upper() != "N" and plr["dif"].upper() != "H" and plr["dif"].upper() != "I":
+        plr["dif"] = input("Enter a valid difficulty: ")
+
+    if plr["dif"].upper() == "E":
+        plr["dif"] = "EASY"
+    if plr["dif"].upper() == "N":
+        plr["dif"] = "NORMAL"
+    if plr["dif"].upper() == "H":
+        plr["dif"] = "HARD"
+    if plr["dif"].upper() == "I":
+        plr["dif"] = "IMPOSSIBLE"
+
+    plr["dif"] = plr["dif"].upper()
+    animatetxt("Selected Difficulty "+ plr["dif"], 3)
+
+difficultyselect()
+
+difCon = input("Are you sure you would like to proceed with difficulty "+ plr["dif"]+"? "+ "[Y/N]: ")
+while difCon.upper() != "Y":
+
+    while difCon.upper() != "Y" and difCon.upper() != "N":
+        difCon = input("Confirm difficulty "+ plr["dif"]+"? "+ "[Y/N]: ")
+
+    if difCon.upper() == "N":
+        difficultyselect()
+        difCon = ""
+
+doTut = input("Would you like to play the tutorial? [Y/N]: ")
 while doTut.upper() != "Y" and doTut.upper() != "N":
     doTut = input("Would you like to play the tutorial? [Y/N]: ")
